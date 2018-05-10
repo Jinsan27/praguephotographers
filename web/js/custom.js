374b30cb63874bc215658e3ca0bc97cf1c16c570
Unknown <nguyenngoct2112@gmail.com>
2018-05-10 15:00:28 +0200

    Basic Mobile Layout

---
+++ web/js/custom.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    items: 1,
+    autoHeight: true,
+    nav: true
+});
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoWidth: true,
+    autoHeight: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    autoplaySpeed: true
+});
