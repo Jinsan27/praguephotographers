c082a9cc00663b15fc00a436e6fd5a2f219a0d52
voralv <vladimir.voral@socialbakers.com>
2019-11-24 11:27:03 +0100

    testimonials improvements v1

---
+++ dist/js/main.js
@@ -0,0 +1,46 @@
+// Carousel for testimonial section
+
+let $carouselTestimonial = $('.carousel-testimonial');
+
+if ($carouselTestimonial.length > 0) {
+
+    $carouselTestimonial.owlCarousel({
+        smartSpeed: 500,
+        items: 1,
+        loop: false,
+        nav:true,
+        singleItem: true,
+        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+        dots:false,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3],
+        stagePadding: 20,
+        margin: 20,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            500:{
+                items:1,
+
+            },
+            600:{
+                items:2,
+
+            },
+            1000:{
+                items:3,
+                stagePadding: 40
+            }
+        }
+    });
+
+    // Custom Button
+    $('.customNextBtn').click(function() {
+        $carouselBlogPost.trigger('next.owl.carousel');
+    });
+    $('.customPreviousBtn').click(function() {
+        $carouselBlogPost.trigger('prev.owl.carousel');
+    });
+}
