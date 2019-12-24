072008eebf48d92636e0377d0d688f8ceea09e36
voralv <vladimir.voral@socialbakers.com>
2019-12-24 14:59:47 +0100

    [webimprovements] - section color, new contact form styles & structure, testimonials

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,18 +20,6 @@ if ($carouselTestimonial.length > 0) {
         responsive:{
             0:{
                 items:1,
-            },
-            500:{
-                items:1,
-
-            },
-            600:{
-                items:2,
-
-            },
-            1000:{
-                items:3,
-                stagePadding: 40
             }
         }
     });
